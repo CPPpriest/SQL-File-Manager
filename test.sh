4f66806df4d666f4168017c555631acc726fb585
priest <mrkilinc22@gmail.com>
2024-02-06 14:23:50 +0300

    bash file performs at %100

--- test.sh
+++ test.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
+
 while true; do
   
   # Prompt the user for the operation name or termination command
-  read -p "Enter operation name (start | end | run | commit | push ): " input
+  read -p "op: " input
   
-  # start command 'start'
-  if [ "$input" = "start" ]; then
-    echo "start mysql server..."
-    sudo systemctl start mysql
-    break
-  fi
-  
-  # termination command 'exit'
-  if [ "$input" = "end" ]; then
+  # end server
+  if [ "$input" = "exit" ]; then
     echo "end mysql server..."
     sudo systemctl stop mysql
     break  # Exit the loops
@@ -21,26 +15,43 @@ while true; do
   
   # Use a case statement to handle different input variations
   case "$input" in
+    
+    start)
+      echo "start mysql server..."
+      sudo systemctl start mysql 
+      ;;
+    
+    end)
+      echo "end mysql server..."
+      sudo systemctl stop mysql
+      ;;
+      
+    status)
+      sudo systemctl status mysql
+      ;;
+      
     run)
       echo "run"
       g++ -o test test.cpp -lmysqlcppconn
-      ./test      
+      ./test
       ;;
+      
     commit)
       git add .
-      
       read -p "commit message: " commit_message
       git commit -m "$commit_message"
-      
       echo "commit -m '$commit_message' ..."
       ;;
+      
     push)
       echo "push"
       git push origin master
       ;;
+      
     *)
-      echo "Invalid input. Please enter 'A' or 'B'."
+      echo "Invalid input. Please enter : (start | end | run | commit | push )"
       ;;
+      
   esac
 done
 
